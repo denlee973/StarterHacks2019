339e4b690747880aa54d4812fcaf77b4c844d431
Denny <denlee973@users.noreply.github.com>
2019-01-13 11:08:37 -0500

    ok this is as far as we're gonna get. I wanted to find the peak of the graph but it was unstable and was essentially just generating random numbers... so I don't know where to go from here.

--- init.py
+++ init.py
@@ -37,29 +37,58 @@ ax.set_xlim(0,2*CHUNK)
 ax2.set_xlim(20,RATE/2)
 plt.setp(ax, xticks=[0, CHUNK, 2*CHUNK], yticks=[0,128,255])
 plt.show(block=False)
+i = 1
 
-while True:
+
+while i<=5:
     data = stream.read(CHUNK)
     data_int = struct.unpack(str(2*CHUNK)+'B',data)
     data_np = np.array((data_int), dtype='b')[::2] + 128
     line.set_ydata(data_np)
+    
     yf = fft(data_int)
-    yoo = max(yf)
-    yf_list = list(yf)
-    yuu = max(yf_list)
-    y_val = yf_list.index(yuu)
-    print yuu, y_val
-    order = yf.argsort()
-    xf_list = list(x_fft)
-    # print xf_list
-    # x_val = xf_list.index(yoo)
+    yfs = yf.argsort()
+    yf_list = list(yfs)
+    
+    
+    
+    print i
+    if i == 1:
+        addingList = yf_list[5:]
+        # print i, addingList
+    else:
+        # print i
+        for j in range(len(yf_list[5:])):
+            addingList[j] += yf_list[j+5]
+            # print "hi"
+        if i == 5: 
+            yuu = max(yf_list[5:])
+            y_val = yf_list.index(yuu)
+            print "done"
+            print yuu, y_val#,yf_list
+            xf_list = list(x_fft[5:])
+            # print xf_list
+            x_val = xf_list[y_val]
+            print "poop"+str(x_val)
+            i=0
+            addingList = []
+            # break
+        # print i, addingList
+    # print "ok"
+    i += 1
+
+    # yuu = max(yf_list[5:])
+    # y_val = yf_list.index(yuu)
+    # print yuu, y_val
+    
+    
     # x1 = x_fft[order]
     # print "x:"+str(len(yf))
     # print "y:"+str(len(x_fft))
     # bro = np.interp(yoo, x_fft, yf)
     # tol = 1e-6;
     # bro = x_val(abs(order-yoo) < tol)
-    # print bro
+
     line_fft.set_ydata(np.abs(yf[0:CHUNK])  / (128 * CHUNK))
     # print "Go!"
     try:
