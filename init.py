10a661043a03037eeccf9849429e58adde3f3e2d
Denny <denlee973@users.noreply.github.com>
2019-01-13 11:26:53 -0500

    last try, can't get the xlist to work because it's too short...

--- init.py
+++ init.py
@@ -30,13 +30,13 @@ x = np.arange(0, 2*CHUNK, 2)
 x_fft = np.linspace(0, RATE, CHUNK)
 
 #line object
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
-line_fft, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
-ax.set_ylim(0,400)
-ax.set_xlim(0,2*CHUNK)
-ax2.set_xlim(20,RATE/2)
-plt.setp(ax, xticks=[0, CHUNK, 2*CHUNK], yticks=[0,128,255])
-plt.show(block=False)
+# line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
+# line_fft, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
+# ax.set_ylim(0,400)
+# ax.set_xlim(0,2*CHUNK)
+# ax2.set_xlim(20,RATE/2)
+# plt.setp(ax, xticks=[0, CHUNK, 2*CHUNK], yticks=[0,128,255])
+# plt.show(block=False)
 i = 1
 
 
@@ -44,7 +44,7 @@ while i<=5:
     data = stream.read(CHUNK)
     data_int = struct.unpack(str(2*CHUNK)+'B',data)
     data_np = np.array((data_int), dtype='b')[::2] + 128
-    line.set_ydata(data_np)
+    # line.set_ydata(data_np)
     
     yf = fft(data_int)
     yfs = yf.argsort()
@@ -64,12 +64,11 @@ while i<=5:
         if i == 5: 
             yuu = max(yf_list[5:])
             y_val = yf_list.index(yuu)
-            print "done"
             print yuu, y_val#,yf_list
-            xf_list = list(x_fft[5:])
-            # print xf_list
-            x_val = xf_list[y_val]
-            print "poop"+str(x_val)
+            # xf_list = list(x_fft[5:])
+            # print len(xf_list)
+            # x_val = xf_list[y_val]
+            # print "poop"+str(x_val)
             i=0
             addingList = []
             # break
@@ -89,15 +88,15 @@ while i<=5:
     # tol = 1e-6;
     # bro = x_val(abs(order-yoo) < tol)
 
-    line_fft.set_ydata(np.abs(yf[0:CHUNK])  / (128 * CHUNK))
+    # line_fft.set_ydata(np.abs(yf[0:CHUNK])  / (128 * CHUNK))
     # print "Go!"
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        # print "Yay!"
-    except:
-        print('Stream stopped')
-        break
+    # try:
+    #     # fig.canvas.draw()
+    #     # fig.canvas.flush_events()
+    #     # print "Yay!"
+    # except:
+    #     print('Stream stopped')
+    #     break
 
 # close the stream gracefully
 stream.stop_stream()
