a7b62a0fc6acc4ea95011ee6e927bd359ddbdb9a
Denny <denlee973@users.noreply.github.com>
2019-01-13 14:36:32 -0500

    more updates to metronome.py

--- init.py
+++ init.py
@@ -30,17 +30,17 @@ x = np.arange(0, 2*CHUNK, 2)
 x_fft = np.linspace(0, RATE, CHUNK)
 
 #line object
-# line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
-# line_fft, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
-# ax.set_ylim(0,400)
-# ax.set_xlim(0,2*CHUNK)
-# ax2.set_xlim(20,RATE/2)
-# plt.setp(ax, xticks=[0, CHUNK, 2*CHUNK], yticks=[0,128,255])
-# plt.show(block=False)
+line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
+line_fft, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
+ax.set_ylim(0,400)
+ax.set_xlim(0,2*CHUNK)
+ax2.set_xlim(20,RATE/2)
+plt.setp(ax, xticks=[0, CHUNK, 2*CHUNK], yticks=[0,128,255])
+plt.show(block=False)
 i = 1
 
 
-while i<=5:
+while True:# i<=5:
     data = stream.read(CHUNK)
     data_int = struct.unpack(str(2*CHUNK)+'B',data)
     data_np = np.array((data_int), dtype='b')[::2] + 128
@@ -52,33 +52,33 @@ while i<=5:
     
     
     
-    print i
-    if i == 1:
-        addingList = yf_list[5:]
-        # print i, addingList
-    else:
-        # print i
-        for j in range(len(yf_list[5:])):
-            addingList[j] += yf_list[j+5]
-            # print "hi"
-        if i == 5: 
-            yuu = max(yf_list[5:])
-            y_val = yf_list.index(yuu)
-            print yuu, y_val#,yf_list
-            # xf_list = list(x_fft[5:])
-            # print len(xf_list)
-            # x_val = xf_list[y_val]
-            # print "poop"+str(x_val)
-            i=0
-            addingList = []
-            # break
-        # print i, addingList
-    # print "ok"
-    i += 1
+    # print i
+    # if i == 1:
+    #     addingList = yf_list[5:]
+    #     # print i, addingList
+    # else:
+    #     # print i
+    #     for j in range(len(yf_list[5:])):
+    #         addingList[j] += yf_list[j+5]
+    #         # print "hi"
+    #     if i == 5: 
+    #         yuu = max(yf_list[5:])
+    #         y_val = yf_list.index(yuu)
+    #         print yuu, y_val#,yf_list
+    #         # xf_list = list(x_fft[5:])
+    #         # print len(xf_list)
+    #         # x_val = xf_list[y_val]
+    #         # print "poop"+str(x_val)
+    #         i=0
+    #         addingList = []
+    #         # break
+    #     # print i, addingList
+    # # print "ok"
+    # i += 1
 
-    # yuu = max(yf_list[5:])
-    # y_val = yf_list.index(yuu)
-    # print yuu, y_val
+    yuu = max(yf_list[5:])
+    y_val = yf_list.index(yuu)
+    print yuu, y_val
     
     
     # x1 = x_fft[order]
@@ -88,15 +88,15 @@ while i<=5:
     # tol = 1e-6;
     # bro = x_val(abs(order-yoo) < tol)
 
-    # line_fft.set_ydata(np.abs(yf[0:CHUNK])  / (128 * CHUNK))
+    line_fft.set_ydata(np.abs(yf[0:CHUNK])  / (128 * CHUNK))
     # print "Go!"
-    # try:
-    #     # fig.canvas.draw()
-    #     # fig.canvas.flush_events()
-    #     # print "Yay!"
-    # except:
-    #     print('Stream stopped')
-    #     break
+    try:
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        # print "Yay!"
+    except:
+        print('Stream stopped')
+        break
 
 # close the stream gracefully
 stream.stop_stream()
--- metronome.py
+++ metronome.py
@@ -2,41 +2,44 @@ import time
 import winsound
 import random
 
-def metronome(bpm, bpb, start, playerNote):
+def metronome(bpm, bpb, start):
      delay = 0.733333*60.0/bpm
      # div = 60.0
      count = 0
      # status = "On beat"
      print delay
      while start==True:
-          rhythmCompare(delay, playerNote,bpb)
+          rhythmCompare(delay,bpb)
           # print status
           if count % bpb == 0:
                winsound.SND_NOSTOP
                winsound.Beep(1200, int(delay*300))
+               print "A"
           else:
                winsound.SND_NOSTOP
                winsound.Beep(600, int(delay*300))
+               print "a"
           count = count + 1
           time.sleep(delay)
      
-def rhythmCompare(delay, playerNote,bpb):
+def rhythmCompare(delay,bpb): #to be modified asfter the frequency input is connected
      status = ""
-     for i in range(0,int(delay*1000),25):
-          if playerNote[0] != playerNote[1] and i%bpb != 0:
-                    status = "Speeding up"
-          elif playerNote[0] != playerNote[1] and i%bpb == 0:
-               status = "On beat"
-          else:
-               status = "Slowing down "
+     for i in range(0,int(delay*100),25):
+          # if playerNote[0] != playerNote[1] and i%bpb != 0:
+          #           status = "Speeding up"
+          # elif playerNote[0] != playerNote[1] and i%bpb == 0:
+          #      status = "On beat"
+          # else:
+          #      status = "Slowing down "
+          status = "Compare"
           print status
-
            
 def main():
      bpm = input("Enter the tempo (bpm) here: ")
      bpb = input("Enter the number of beats in a measure: ")
      start = True
+     # playerNotes = [0,1] Will be updated after frequency input is connected
      for i in range(0,25):
-          metronome(bpm, bpb, start, playerNotes)
+          metronome(bpm, bpb, start)
 
 main()
